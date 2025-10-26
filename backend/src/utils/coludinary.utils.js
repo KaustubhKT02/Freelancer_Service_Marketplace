d22b56ac9e4cfa9429d3e6cee49077bec29c53cd
KaustubhKT02 <kaustu8bhtripathi36@gmail.com.com>
2025-10-26 11:58:10 +0530

    Add multer for file uploads and integrate Cloudinary for image storage; configure multer storage settings and implement Cloudinary upload utility

---
+++ backend/src/utils/coludinary.utils.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+// Configure Cloudinary with credentials from environment variables
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+// 
+
+const uploadCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+
+        // Upload the file to Cloudinary
+        const result = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+        });
+        console.log('Cloudinary upload result:', result.url);
+        return result;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // Delete the local file in case of error
+        console.error('Error uploading to Cloudinary:', error);
+        throw error;
+    }
+}
+
+export {uploadCloudinary};
