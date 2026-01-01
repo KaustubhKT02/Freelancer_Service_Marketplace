81ba47eb7582812353c2f8e9dc5125b6f937289f
KaustubhKT02 <kaustubhtripathi36@gmail.com>
2026-01-01 12:09:42 +0530

    feat: update frontend dependencies and integrate Tailwind CSS
    - Added Tailwind CSS and related dependencies to package.json
    - Updated App component to use Tailwind CSS for styling
    - Created index.css to import Tailwind CSS styles
    - Configured Vite to use Tailwind CSS plugin and updated API proxy URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api' : 'http://localhost:5000',
+      '/api' : process.env.VITE_BASE_URL,
     }
   },
   
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
